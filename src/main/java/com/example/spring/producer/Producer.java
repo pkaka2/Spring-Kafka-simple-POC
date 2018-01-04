002e35f5c26fe8e8a141dcfbcf01fa6fac9bc058
Pr@deEp <pradeep.kakarlaj16@gmail.com>
2018-01-04 15:40:52 -0600

    intial commit

---
+++ src/main/java/com/example/spring/producer/Producer.java
@@ -0,0 +1,19 @@
+package com.example.spring.producer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+
+public class Producer {
+	  private static final Logger LOGGER = LoggerFactory.getLogger(Producer.class);
+
+	  @Autowired
+	  private KafkaTemplate<String, String> kafkaTemplate;
+	  
+	  public void send(String topic, String payload) {
+		  payload = "Hello world";
+	    LOGGER.info("sending payload='{}' to topic='{}'", payload, topic);
+	    kafkaTemplate.send(topic, payload);
+	  }
+}
